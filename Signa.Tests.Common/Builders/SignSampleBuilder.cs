9521c65b20da5ab28802bc4f017389b8ee4fe6a7
Júlio César Batista <jcb.1611@gmail.com>
2015-02-16 14:11:38 -0200

    37: Testes de treinamento e reconhecimento da classe Svm

---
+++ Signa.Tests.Common/Builders/SignSampleBuilder.cs
@@ -0,0 +1,46 @@
+﻿using Signa.Model;
+
+namespace Signa.Tests.Common.Builders
+{
+    public class SignSampleBuilder
+    {
+        private double[] anglesBetweenFingers;
+        private double[] palmNormal;
+        private double[] handDirection;
+
+        public SignSampleBuilder()
+        {
+            anglesBetweenFingers = new DoubleArrayBuilder().WithSize(4).Build();
+            palmNormal = new DoubleArrayBuilder().WithSize(3).Build();
+            handDirection = new DoubleArrayBuilder().WithSize(3).Build();
+        }
+
+        public SignSampleBuilder WithAnglesBetweenFingers(double[] anglesBetweenFingers)
+        {
+            this.anglesBetweenFingers = anglesBetweenFingers;
+            return this;
+        }
+
+        public SignSampleBuilder WithPalmNormal(double[] palmNormal)
+        {
+            this.palmNormal = palmNormal;
+            return this;
+        }
+
+        public SignSampleBuilder WithHandDirection(double[] handDirection)
+        {
+            this.handDirection = handDirection;
+            return this;
+        }
+
+        public SignSample Build()
+        {
+            return new SignSample
+            {
+                AnglesBetweenFingers = anglesBetweenFingers,
+                PalmNormal = palmNormal,
+                HandDirection = handDirection
+            };
+        }
+    }
+}
---
+++ Signa.Tests/Recognizer/SvmTest.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Signa.Model;
+using Signa.Recognizer;
+using Signa.Tests.Common.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace Signa.Tests.Recognizer
+{
+    [TestClass]
+    public class SvmTest
+    {
+        [TestMethod]
+        public void recognizing_without_trainning_throw_an_error()
+        {
+            var sample = new SignSampleBuilder().Build();
+            Action recognizeCall = () => Svm.Instance.Recognize(sample);
+
+            recognizeCall.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public void recognizing_an_input()
+        {
+            const int signCount = 3;
+            const int samplesPerSign = 2;
+            const int signResultIndex = 2;
+
+            GivenATrainedAlgorithm(signCount, samplesPerSign);
+            
+            var result = Svm.Instance.Recognize(BuildSignSampleByIndex(signResultIndex));
+
+            result.Should().Be(signResultIndex);
+        }
+
+        private static void GivenATrainedAlgorithm(int signCount, int samplesPerSign)
+        {
+            var signs = GivenACollectionOfSigns(signCount, samplesPerSign);
+            var trainningData = new SvmTrainningData(signs);
+            Svm.Instance.Train(trainningData);
+        }
+
+        private static ICollection<Sign> GivenACollectionOfSigns(int signCount, int samplesPerSign)
+        {
+            var signs = new SignCollectionBuilder()
+                            .WithSize(signCount)            
+                            .WithSampleCount(samplesPerSign)
+                            .WithSampleGenerator(i => BuildSignSampleByIndex(i))
+                            .Build();
+            return signs;
+        }
+
+        private static SignSample BuildSignSampleByIndex(int index)
+        {
+            return new SignSampleBuilder()
+                .WithAnglesBetweenFingers(new double[] { index, index, index, index })
+                .WithPalmNormal(new double[] { index, index, index })
+                .WithHandDirection(new double[] { index, index, index })
+                .Build();
+        }
+    }
+}
