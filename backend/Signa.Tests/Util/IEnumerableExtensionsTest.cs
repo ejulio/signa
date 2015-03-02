bb2a07e6faf12cc30e4d9dc551d79e48aff4f344
Júlio César Batista <jcb.1611@gmail.com>
2015-03-02 19:43:36 -0300

    46: Implementação de Extension Method para concatenar arrays

---
+++ backend/Signa.Tests/Util/IEnumerableExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Signa.Util;
+
+namespace Signa.Tests.Util
+{
+    [TestClass]
+    public class IEnumerableExtensionsTest
+    {
+        [TestMethod]
+        public void concatenating_arrays_of_double()
+        {
+            var arrays = new[]
+            {
+                new [] { 0.8, 0.7, 0.5 },
+                new [] { 1.4, 1.6, 1.8 }
+            };
+
+            var concatenatedArray = arrays.Concatenate().ToArray();
+
+            var expectedArray = new[] { 0.8, 0.7, 0.5, 1.4, 1.6, 1.8 };
+
+            concatenatedArray.Should().HaveSameCount(expectedArray);
+            concatenatedArray.Should().ContainInOrder(expectedArray);
+        }
+    }
+}
---
+++ backend/Signa/Util/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Signa.Util
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<TInput> Concatenate<TInput>(this IEnumerable<IEnumerable<TInput>> enumerables)
+        {
+            IEnumerable<TInput> collection = new TInput[0];
+
+            return enumerables.Aggregate(collection, (current, enumerable) => current.Concat(enumerable));
+        }
+    }
+}
