f55f3dd0df771ca5a3e4964eae2546b0425f89c1
Júlio César Batista <jcb.1611@gmail.com>
2015-03-18 20:27:55 -0300

    0:Funções matemáticas referentes a vetores

---
+++ backend/Dominio/Matematica/DoubleArrayExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Dominio.Matematica
+{
+    public static class DoubleArrayExtensions
+    {
+        public static double Magnitude(this double[] vetor)
+        {
+            var xAoQuadrado = vetor[0] * vetor[0];
+            var yAoQuadrado = vetor[1] * vetor[1];
+            var zAoQuadrado = vetor[2] * vetor[2];
+            return Math.Sqrt(xAoQuadrado + yAoQuadrado + zAoQuadrado);
+        }
+
+        public static double ProdutoCom(this double[] vetor1, double[] vetor2)
+        {
+            var produtoDeX = vetor1[0] * vetor2[0];
+            var produtoDeY = vetor1[1] * vetor2[1];
+            var produtoDeZ = vetor1[2] * vetor2[2];
+
+            return produtoDeX + produtoDeY + produtoDeZ;
+        }
+
+        public static double AnguloAte(this double[] vetor1, double[] vetor2)
+        {
+            var magnitude = vetor1.Magnitude() * vetor2.Magnitude();
+
+            if (magnitude == 0.0)
+                return 0;
+
+            return Math.Acos(vetor1.ProdutoCom(vetor2) / magnitude);
+        }
+    }
+}
---
+++ backend/Testes/Unidade/Matematica/DoubleArrayExtensionsTeste.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Dominio.Matematica;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testes.Unidade.Matematica
+{
+    [TestClass]
+    public class DoubleArrayExtensions
+    {
+        [TestMethod]
+        public void calculando_a_magnitude_de_um_vetor()
+        {
+            var vetor = new[] { 1.0, 2.0, 3.0 };
+
+            var magnitude = vetor.Magnitude();
+
+            magnitude.Should().Be(Math.Sqrt(14.0));
+        }
+
+        [TestMethod]
+        public void calculando_o_produto_entre_dois_vetores()
+        {
+            var vetor1 = new[] { 1.0, 2.0, 3.0 };
+            var vetor2 = new[] { 2.0, 3.0, 1.0 };
+
+            var produto = vetor1.ProdutoCom(vetor2);
+
+            produto.Should().Be(11.0);
+        }
+
+        [TestMethod]
+        public void calculando_o_angulo_entre_dois_vetores()
+        {
+            var vetor1 = new[] { 1.0, 2.0, 3.0 };
+            var vetor2 = new[] { 2.0, 3.0, 1.0 };
+
+            var angulo = vetor1.AnguloAte(vetor2);
+            var produto = vetor1.ProdutoCom(vetor2);
+            var magnitude = vetor1.Magnitude() * vetor2.Magnitude();
+
+            angulo.Should().Be(Math.Acos(produto / magnitude));
+        }
+
+        [TestMethod]
+        public void calculando_o_angulo_entre_dois_vetores_quando_um_vetor_eh_a_origem()
+        {
+            var vetor1 = new[] { 1.0, 2.0, 3.0 };
+            var vetor2 = new[] { 0.0, 0.0, 0.0 };
+
+            var angulo = vetor1.AnguloAte(vetor2);
+
+            angulo.Should().Be(0.0);
+        }
+    }
+}
